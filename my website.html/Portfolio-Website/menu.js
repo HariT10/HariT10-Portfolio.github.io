491f5f334d7f80e1a7e02ad082d9353607db0c64
Harishan Thilakanathan <79531856+HariT10@users.noreply.github.com>
2025-01-06 16:24:31 -0500

    Add files via upload

---
+++ my website.html/Portfolio-Website/menu.js
@@ -0,0 +1,265 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu'); // Select the menu
+    menu.classList.toggle('nav-active'); // Toggle the active class
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "src" attribute
+var img = document.getElementById("myImage");
+var modalImg = document.getElementById("imgInModal");
+
+
+function openModal1() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 1.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal1() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+function openModal2() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 2.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal2() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+function openModal3() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 3.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal3() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+function openModal4() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 4.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal4() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+function openModal5() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 5.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal5() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+function openModal6() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 6.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal6() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+function openModal7() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 7.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal7() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+
+function openModal8() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 8.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal8() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+function openModal9() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 9.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal9() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+function openModal10() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 10.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal10() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+function openModal11() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 11.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal11() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+function openModal12() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 12.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal12() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+function openModal13() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 13.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal13() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+
+function openModal14() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 14.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal14() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+
+
+function openModal15() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 15.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal15() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+
+function openModal16() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 16.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+
+function closeModal16() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+
+function openModal17() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 17.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+
+function closeModal17() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+
+function openModal17() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 17.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal17() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
+function openModal18() {
+    modal.style.display = "flex"; // Show the modal
+    modalImg.src = "image 18.1.png" // Use the same image source, or change it to a higher resolution image here
+}
+
+function closeModal18() {
+    modal.style.display = "none"; // Hide the modal
+}
+
+
